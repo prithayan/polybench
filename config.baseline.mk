139670d662262a5fe1000b827d5d20d2705af0c4
Prithayan Barua <prithayan@gmail.com>
2020-08-27 00:17:13 -0400

    Update config

--- config.baseline.mk
+++ config.baseline.mk
@@ -1,4 +1,5 @@
-LLVM_HOME=/localscratch/pbarua3/IBM/llvm-project/build/bin
+#LLVM_HOME=/localscratch/pbarua3/IBM/llvm-project/build/bin
 CC=${LLVM_HOME}/clang
 UNROLLJAM_OPTIONS=-fexperimental-new-pass-manager -O3 -fno-inline-functions -fno-slp-vectorize -fno-vectorize  -Rpass=unroll
-CFLAGS=-DPOLYBENCH_PAPI -I/${PAPI_DIR}/include -L/${PAPI_DIR}/lib -lpapi -DLARGE_DATASET -DPOLYBENCH_USE_RESTRICT -DPOLYBENCH_USE_SCALAR_LB -DUNROLL_1=${UNROLL1} -DUNROLL_2=${UNROLL2} ${UNROLLJAM_OPTIONS}
+CFLAGS=-DPOLYBENCH_TIME -DEXTRALARGE_DATASET -DPOLYBENCH_USE_RESTRICT -DPOLYBENCH_USE_SCALAR_LB -DUNROLL_1=${UNROLL1} -DUNROLL_2=${UNROLL2} ${UNROLLJAM_OPTIONS}
+#-DPOLYBENCH_PAPI -I/${PAPI_DIR}/include -L/${PAPI_DIR}/lib -lpapi -DEXTRALARGE_DATASET
--- config.mk
+++ config.mk
@@ -1,4 +1,5 @@
-LLVM_HOME=/localscratch/pbarua3/IBM/llvm-project/build/bin
+#LLVM_HOME=/localscratch/pbarua3/IBM/llvm-project/build/bin
 CC=${LLVM_HOME}/clang
 UNROLLJAM_OPTIONS=-fexperimental-new-pass-manager -O3 -fno-inline-functions -fno-slp-vectorize -fno-vectorize -mllvm -enable-simple-unroll-and-jam -mllvm -enable-unroll-and-jam  -mllvm -allow-unroll-and-jam   -mllvm -unroll-and-jam-skip-dependency-checks -mllvm -allow-simple-unroll-and-jam -mllvm -enable-perfect-nest-unroll-and-jam  -mllvm -da-disable-delinearization-checks    -Rpass=unroll
-CFLAGS=-DPOLYBENCH_PAPI -I/${PAPI_DIR}/include -L/${PAPI_DIR}/lib -lpapi -DLARGE_DATASET -DPOLYBENCH_USE_RESTRICT -DPOLYBENCH_USE_SCALAR_LB ${UNROLLJAM_OPTIONS}
+CFLAGS=-DPOLYBENCH_TIME -DEXTRALARGE_DATASET -DPOLYBENCH_USE_RESTRICT -DPOLYBENCH_USE_SCALAR_LB ${UNROLLJAM_OPTIONS}
+#-DPOLYBENCH_PAPI -I/${PAPI_DIR}/include -L/${PAPI_DIR}/lib -lpapi 
