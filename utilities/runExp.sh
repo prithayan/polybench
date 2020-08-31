ef1a0157f6bf0f68088d0bbc497f23875a0ae5c2
Prithayan Barua <prithayan@gatech.edu>
2020-08-31 00:37:03 -0400

    run exp

--- utilities/runExp.sh
+++ utilities/runExp.sh
@@ -13,19 +13,30 @@ PerfCsvOut="$DIR/tmp"
 PerfTempOut="temp.perfout"
 ProfileCmd="perf stat -e cycles,instructions,cache-references,cache-misses,L1-dcache-loads,L1-dcache-load-misses,branches,branch-misses,L1-icache-loads,L1-icache-load-misses -x, -o  $PerfTempOut ./$kernel "
 ProfileCmd="timeout 120m bash $DIR/utilities/time_benchmark.sh ./$kernel"
-ProfileCmd="./$kernel"
+#ProfileCmd="./$kernel"
 #ProfileCmd="perf stat -e cycles,L1-dcache-loads,L1-dcache-load-misses -x, -o  $PerfTempOut ./$kernel "
 #if grep -q "pragma unroll_and_jam" *.c; then
   echo found
   echo $targetDir >> $PerfCsvOut ;  
   echo $targetDir >>  $OutputMakeBase ;  
-  echo "BASELINE=========" >> $OutputMakeBase ;
-	make clean; timeout 45m make -f Makefile.baseline 2>>$OutputMakeBase ; O0=`$ProfileCmd` ;
   #O0=`grep "L1\|cycles" $PerfTempOut | cut -f1 -d',' | perl -p  -e 's/\R/,/g;'`;
   mv $kernel ${kernel}_baseline
   #perl -p  -e 's/\R//g;'  $PerfTempOut >> $PerfCsvOut ; echo "" >> $PerfCsvOut
   echo $targetDir >>  $OutputMake ;  
+  export VECT_OPTIONS="-fno-slp-vectorize -fno-vectorize"
+  echo "BASELINE=========" >> $OutputMakeBase ;
+	make clean; timeout 45m make -f Makefile.baseline 2>>$OutputMakeBase ; O0=`$ProfileCmd` ;
   echo "UnJ Pass=========" >> $OutputMake
 	make clean; timeout 45m make -f Makefile 2>>$OutputMake ; O1=`$ProfileCmd` ;
+  export VECT_OPTIONS="-fno-vectorize"
+  echo "UnJ Pass=========" >> $OutputMake
+  echo "BASELINE=========" >> $OutputMakeBase ;
+	make clean; timeout 45m make -f Makefile.baseline 2>>$OutputMakeBase ; O0_2=`$ProfileCmd` ;
+	make clean; timeout 45m make -f Makefile 2>>$OutputMake ; O2=`$ProfileCmd` ;
+  export VECT_OPTIONS=""
+  echo "UnJ Pass=========" >> $OutputMake
+  echo "BASELINE=========" >> $OutputMakeBase ;
+	make clean; timeout 45m make -f Makefile.baseline 2>>$OutputMakeBase ; O0_3=`$ProfileCmd` ;
+	make clean; timeout 45m make -f Makefile 2>>$OutputMake ; O3=`$ProfileCmd` ;
   #O1=`grep "L1\|cycles" $PerfTempOut | cut -f1 -d',' | perl -p  -e 's/\R/,/g;'` ;
-	echo -n $targetDir >> $OUTFILE; echo ",$O0,$O1" >> $OUTFILE; 
+	echo -n $kernel >> $OUTFILE; echo ",$O0,$O1,$O0_2,$O2,$O0_3,$O3" >> $OUTFILE; 
